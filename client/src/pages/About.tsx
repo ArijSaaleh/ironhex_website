b870a2f3b321d102b54913c7f5b5993079dfdb01
ArijSaaleh <arij.saleh.pro@gmail.com>
2025-10-28 20:08:48 +0100

    about

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -18,7 +18,7 @@ export default function About(){
         <div className="max-w-4xl mx-auto px-6 text-center">
           <h2 className="text-2xl font-bold text-gray-900 dark:text-white">{t('about.story.title')}</h2>
           <p className="mt-4 text-gray-700 dark:text-gray-300">{t('about.story.p1')}</p>
-          <p className="mt-2 text-gray-700 dark:text-gray-300">{t('about.story.p2')}</p>
+          
         </div>
       </section>
 
